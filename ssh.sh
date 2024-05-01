6f7bb53b363642131d11cdfc522647bd61a82de7
bobnat1 <natehuser@gmail.com>
2024-04-30 22:13:25 -0700

    create user oriented bash script

---
+++ ssh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+file_id=$(<"./.env")
+### ssh into session with a sub shell
+# echo "ssh -i myKey.pem ec2-user@$file_id" | tr -d '"' | bash - 
+### ssh into session with current shell
+eval $(echo "ssh -i myKey.pem ec2-user@$file_id" | tr -d '"') 
